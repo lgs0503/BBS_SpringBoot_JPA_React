bcd4bdeb848d4aaf4c3930edc638a8258036c13b
lgs0503 <tjrdl0503@naver.com>
2022-05-22 11:32:19 +0900

    war 배포 설정

--- src/main/java/lgs/bbs/BbsApplication.java
+++ src/main/java/lgs/bbs/BbsApplication.java
@@ -3,9 +3,16 @@ package lgs.bbs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class BbsApplication {
+public class BbsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BbsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BbsApplication.class, args);
